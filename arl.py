a119d05e91810b529b829e0c087842e2691b317f
Bela <bela.schinke@gmail.com>
2025-01-28 19:51:38 +0100

    change syntax

--- arl.py
+++ arl.py
@@ -1,4 +1,9 @@
-from .utils import *
+from embeddings.ARL_Adv.utils import *
+
+# ----------------------------------
+# IMPORTANT: Enable v1 compatibility
+# ----------------------------------
+tf.compat.v1.disable_eager_execution()
 
 
 class ARL:
@@ -7,12 +12,18 @@ class ARL:
         self.neg_num = args['ns']
         # learning rate
         self.lr = args['lr']
-        self.x_init = tf.contrib.layers.xavier_initializer()
+
+        # Replace the old tf.contrib.layers.xavier_initializer with a Keras initializer
+        from tensorflow.keras.initializers import GlorotUniform
+        self.x_init = GlorotUniform()
 
     def set_session(self, sess):
         self.sess = sess
 
     def build(self, w_embed, c_embed):
+        """
+        Main build function: sets up variables, placeholders, forward pass, and optimizer.
+        """
         self.define_word_embed(w_embed)
         self.define_cluster_embed(c_embed)
         self.define_inputs()
@@ -21,51 +32,127 @@ class ARL:
 
     @staticmethod
     def get_variable(name, init, train):
-        initer = tf.constant_initializer(init)
-        var = tf.get_variable(name=name, shape=init.shape, initializer=initer, trainable=train)
+        """
+        Minimal wrapper to create a tf.compat.v1 variable using a constant initializer.
+        """
+        # Switch from tf.get_variable to tf.compat.v1.get_variable
+        var = tf.compat.v1.get_variable(
+            name=name,
+            shape=init.shape,
+            initializer=tf.compat.v1.constant_initializer(init),
+            trainable=train
+        )
         return var
 
     def define_word_embed(self, init):
+        """
+        Create a trainable word embedding variable with a 'pad' row of zeros on top.
+        """
+        # pad row
         pad = tf.zeros(shape=(1, tf.shape(init)[1]), name='w_padding', dtype=tf.float32)
+
+        # get_variable (trainable)
         emb = self.get_variable('w_embed', init, True)
+
+        # Concat zero row to the top
         self.w_embed = tf.concat([pad, emb], axis=0, name='w_embed_concat')
 
     def define_cluster_embed(self, init):
+        """
+        Create a trainable cluster embedding variable.
+        """
         self.c_embed = self.get_variable('c_embed', init, True)
 
     def define_inputs(self):
-        self.is_train = tf.placeholder_with_default(True, (), name='is_train')
-        lk, ph = tf.nn.embedding_lookup, tf.placeholder
+        """
+        Define placeholders for training (pos/neg sequences).
+        """
+        # Switch to tf.compat.v1 placeholders
+        self.is_train = tf.compat.v1.placeholder_with_default(True, (), name='is_train')
+
+        # We'll store shapes in normal Python variables
         shape = (None, None)
-        self.p_seq = ph(tf.int32, shape, name='p_seq')
-        self.n_seqs = [ph(tf.int32, shape, name='n_seq_{}'.format(i)) for i in range(self.neg_num)]
+
+        # placeholders for sequences
+        self.p_seq = tf.compat.v1.placeholder(tf.int32, shape=shape, name='p_seq')
+        self.n_seqs = [
+            tf.compat.v1.placeholder(tf.int32, shape=shape, name=f'n_seq_{i}')
+            for i in range(self.neg_num)
+        ]
+
+        # lookups
         with tf.name_scope('lookup_pos'):
-            self.p_lk = lk(self.w_embed, self.p_seq)
+            self.p_lk = tf.nn.embedding_lookup(self.w_embed, self.p_seq)
             self.p_rep = tf.reduce_mean(self.p_lk, axis=1, keepdims=False)
+
         with tf.name_scope('lookup_neg'):
-            self.n_lks = [lk(self.w_embed, n, name='n_rep_{}'.format(i)) for i, n in enumerate(self.n_seqs)]
-            self.n_reps = tf.concat([tf.reduce_mean(n_lk, axis=1, keepdims=False) for n_lk in self.n_lks], axis=0)
+            self.n_lks = [
+                tf.nn.embedding_lookup(self.w_embed, n, name=f'n_rep_{i}')
+                for i, n in enumerate(self.n_seqs)
+            ]
+            # Concatenate the averaged negative embeddings
+            self.n_reps = tf.concat(
+                [tf.reduce_mean(n_lk, axis=1, keepdims=False) for n_lk in self.n_lks],
+                axis=0
+            )
 
     def define_optimizer(self):
-        self.optimizer = tf.train.AdamOptimizer(tf.train.exponential_decay(
-            learning_rate=self.lr, global_step=0, decay_steps=50, decay_rate=0.99))
+        """
+        Create an Adam optimizer with exponential decay, then build the gradient update op.
+        """
+        global_step = tf.compat.v1.train.get_or_create_global_step()
+
+        lr_decayed = tf.compat.v1.train.exponential_decay(
+            learning_rate=self.lr,
+            global_step=global_step,
+            decay_steps=50,
+            decay_rate=0.99,
+            staircase=False
+        )
+
+        self.optimizer = tf.compat.v1.train.AdamOptimizer(lr_decayed)
+
+        # compute gradients
         self.sim_gvs = self.optimizer.compute_gradients(self.sim_loss)
-        self.sim_opt = self.optimizer.apply_gradients(self.sim_gvs, name='sim_op')
+
+        # apply gradients
+        self.sim_opt = self.optimizer.apply_gradients(self.sim_gvs, name='sim_op', global_step=global_step)
 
     def get_loss_with(self):
+        """
+        Compute cluster probabilities (pc_probs), as well as pairwise and pointwise loss components.
+        """
         with tf.name_scope('get_loss'):
             c_emb = self.c_embed
+
             pos_d, neg_d = self.p_rep, self.n_reps
-            pc_probs, pos_recon = self.get_c_probs_and_recon_d(pos_d, c_emb, name='recon_d')
+            pc_probs, pos_recon = self.get_c_probs_and_recon_d(
+                pos_d, c_emb, name='recon_d'
+            )
+
+            # L2-normalize the relevant vectors
             pos_d, pos_recon, neg_d = l2_norm_tensors(pos_d, pos_recon, neg_d)
+
+            # dot product of pos_d and pos_recon
             pos_dr_sim = inner_dot(pos_d, pos_recon, keepdims=True)
+
+            # dot product of pos_d with each negative sample
             pos_neg_sim = tf.matmul(pos_d, tf.transpose(neg_d))
+
+            # margin-based loss
             pairwise_margin = tf.maximum(0., 1. - pos_dr_sim + pos_neg_sim)
             pairwise = tf.reduce_mean(pairwise_margin)
+
+            # average positive similarity
             pointwise = tf.reduce_mean(pos_dr_sim)
+
         return pc_probs, pairwise, pointwise
 
     def get_c_probs_and_recon_d(self, d_emb, c_emb, name):
+        """
+        For a given document embedding `d_emb`, compute the cluster probabilities
+        and reconstruct the doc embedding by weighting the cluster embeddings.
+        """
         with tf.name_scope(name):
             c_score = tf.matmul(d_emb, tf.transpose(c_emb))
             c_probs = tf.nn.softmax(c_score, axis=1)
@@ -73,29 +160,64 @@ class ARL:
         return c_probs, recon
 
     def forward(self):
+        """
+        Run the model forward pass and define the final similarity loss.
+        """
         self.pc_probs, pairwise, pointwise = self.get_loss_with()
         J1 = pairwise - pointwise
         self.sim_loss = J1
 
     def get_fd_by_batch(self, p_batch, n_batches=None):
-        p_seq = [d.tokenids for d in p_batch]
-        pairs = [(self.p_seq, p_seq)]
+        # 1) Extract token sequences
+        p_seq_list = [d.tokenids for d in p_batch]
+        # 2) Pad the positive batch
+        p_seq_padded = pad_batch(p_seq_list, dtype='int32')
+
+        # 3) Build feed pairs
+        pairs = [(self.p_seq, p_seq_padded)]
+
+        # 4) For negative batches, also pad
         if n_batches is not None:
-            n_seqs = [[d.tokenids for d in n_batch] for n_batch in n_batches]
-            pairs += list(zip(self.n_seqs, n_seqs))
+            for placeholder, neg_batch in zip(self.n_seqs, n_batches):
+                n_seq_list = [d.tokenids for d in neg_batch]
+                n_seq_padded = pad_batch(n_seq_list, dtype='int32')
+                pairs.append((placeholder, n_seq_padded))
+
         return dict(pairs)
 
     def train_step(self, pos, negs):
+        """
+        Single training step on one batch of data (pos + negs).
+        """
         fd = self.get_fd_by_batch(pos, negs)
         self.sess.run(self.sim_opt, feed_dict=fd)
 
     def predict(self, batch):
+        """
+        Run forward pass in inference mode (is_train=False),
+        returning cluster assignments and document embeddings.
+        """
         fd = self.get_fd_by_batch(batch)
         fd[self.is_train] = False
-        return np.argmax(self.sess.run(self.pc_probs, feed_dict=fd), axis=1).reshape(-1)
+        # We fetch the cluster probabilities and the doc embeddings (p_rep)
+        cluster_probs, doc_embeddings = self.sess.run(
+            [self.pc_probs, self.p_rep],
+            feed_dict=fd
+        )
+        # Convert cluster probabilities into the actual cluster labels
+        clusters = np.argmax(cluster_probs, axis=1)
+        return clusters, doc_embeddings
 
     def save(self, file):
-        tf.train.Saver().save(self.sess, file)
+        """
+        Save model variables to a checkpoint.
+        """
+        saver = tf.compat.v1.train.Saver()
+        saver.save(self.sess, file)
 
     def load(self, file):
-        tf.train.Saver().restore(self.sess, file)
+        """
+        Restore model variables from a checkpoint.
+        """
+        saver = tf.compat.v1.train.Saver()
+        saver.restore(self.sess, file)
--- utils.py
+++ utils.py
@@ -1,16 +1,36 @@
 import tensorflow as tf
 import numpy as np
 
-
 def inner_dot(a, b, axis=-1, keepdims=False, name=None):
-    with tf.name_scope(name):
+    """
+    Computes the sum of elementwise multiplication of a and b along the given axis.
+    """
+    with tf.name_scope(name or "inner_dot"):
         return tf.reduce_sum(a * b, axis=axis, keepdims=keepdims)
 
-
 def l2_norm_tensors(*tensors):
+    """
+    L2-normalizes each tensor in `tensors` along the last dimension.
+    Returns the same number of tensors.
+    """
     return apply_tensors(lambda t: tf.nn.l2_normalize(t, axis=-1), *tensors)
 
-
 def apply_tensors(func, *tensors):
+    """
+    Applies a given function `func` to each tensor in `tensors`.
+    Returns a list if multiple tensors are provided, otherwise returns a single value.
+    """
     res = [func(t) for t in tensors]
     return res if len(res) > 1 else res[0]
+
+def pad_batch(list_of_seqs, dtype='int32'):
+    """
+    Given a list of integer lists (variable-length), pad them to the same max length.
+    Returns a 2D NumPy array of shape [batch_size, max_length].
+    """
+    max_len = max(len(seq) for seq in list_of_seqs) if list_of_seqs else 0
+    batch_size = len(list_of_seqs)
+    arr = np.zeros((batch_size, max_len), dtype=dtype)
+    for i, seq in enumerate(list_of_seqs):
+        arr[i, :len(seq)] = seq
+    return arr
